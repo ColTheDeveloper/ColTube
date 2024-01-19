c845f8d12080e10fa8e340e7f7b78194f3be655e
Aremu Olakunle Umaru <colakunleumaru@gmail.com>
2024-01-19 23:26:10 +0100

    added the tag page

---
+++ frontend/src/pages/Tag/Tag.tsx
@@ -0,0 +1,54 @@
+import axios from "axios";
+import Card from "../../components/Card/Card";
+import "./Home.css"
+import { useEffect, useState } from "react";
+import { useDispatch} from "react-redux";
+import { closeNavbar } from "../../redux/navbarSlide";
+import Spinner from "../../components/Spinner/Spinner"
+import { videoType } from "../../types/pagePropsTypes";
+
+type VideoPropType={
+    tag:string
+}
+
+const Tag=({tag}:VideoPropType)=>{
+    const [videos,setVideos]=useState<videoType[] | null>(null)
+    const [isLoading,setIsLoading]=useState(false)
+    const dispatch=useDispatch()
+
+    
+    useEffect(()=>{
+        dispatch(closeNavbar())
+        const loadVideo=async()=>{
+            setIsLoading(true)
+            const response= await axios.get(`http://localhost:2500/api/videos/${tag}`,{withCredentials:true})
+            console.log(response)
+            setIsLoading(false)
+            setVideos(response.data)
+        }
+        loadVideo()
+    },[tag,dispatch])
+    return(
+        <div className="Home Body">
+            {isLoading?
+                
+                <Spinner />
+                
+            :
+                videos?.length===0?
+                    <div className="Loading">
+                        <h1>No Video Available</h1>
+                    </div>
+                :
+                    <div className="Card-container">
+                        {videos?.map((video)=>{
+                            return(
+                                <Card video={video} />
+                            )
+                        })} 
+                    </div>
+            }
+        </div>
+    )
+}
+export default Tag;
